8ddd3fe33ea33464ee3ab15d07e3d7dc11acab2d
Louis Ryan <louisryan@me.com>
2014-06-27 21:32:17 +0100

    Database Structure

---
+++ datatau.sql
@@ -0,0 +1,24 @@
+
+-- ----------------------------
+--  Table structure for `links`
+-- ----------------------------
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE `links` (
+  `url`  varchar(100) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `scrape_time` datetime NOT NULL,
+  `points` INT NOT NULL,
+  `user` varchar(20) NOT NULL,
+  PRIMARY KEY (`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+-- ------------------------------------
+--  Table structure for next_page_links
+-- ------------------------------------
+DROP TABLE IF EXISTS `next_page_links`;
+CREATE TABLE `next_page_links` (
+	`next_page_id` INT NOT NULL AUTO_INCREMENT,
+	`next_page_url` varchar(60),
+	PRIMARY KEY (next_page_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
